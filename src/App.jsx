7be0f964ba222b3dbefe464334181911e8cb778b
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2021-02-23 11:52:58 -0500

    heroku-deploy v3.11.10 ...2

--- src/App.jsx
+++ src/App.jsx
@@ -25,19 +25,12 @@ const App = () => {
     return <ErrorMessage error={error} />
   }
 
-  const healthCheck = health('/health')
-
   return (
     <Router>
       <Switch>
         <Route exact path="/">
           <PokemonList pokemonList={pokemonList} />
         </Route>
-
-        {/*         <Route path="/health">
-          {healthCheck}
-        </Route>
- */}
         <Route
           path="/pokemon/:name"
           render={(routeParams) => {
